d6e03d3a85dfa007ce6973b91896c58dca086f63
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-10 11:33:12 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,14 +1,18 @@
+# this "wrapper" script calls various other scripts that each achieve one subproject (see README) in the overall analysis
+
 chmod +x *.sh
 
+# get/produce data, and set HyperTraPS running
 ./subset.sh
 ./parse-trees.sh
 ./hypertraps-long.sh
 ./synth.sh
 
-# only run this when MCMC chains have finished
+# only run these when MCMC chains have finished
 #./analysis.sh
 #./predict.sh
 
+# run these for all command-line visualisation
 #cd Visual
 #chmod +x plot-all.sh
 #./plot-all.sh
