6b9b215039cde06e841e92cc53b5f0a58e75c7b5
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-10 11:39:53 +0100

    Update parse-converge.sh

--- parse-converge.sh
+++ parse-converge.sh
@@ -1,1 +1,2 @@
+# rather messily gets the output of C code for convergence and extracts distributions of the Gellman-Rubin statistic for plotting
 grep "Rhat" $1 | awk 'BEGIN{for(i=0;i<100;i++)hist[i] = 0;}{hist[int($2*10)]++;}END{for(i=0;i<100;i++){if(hist[i]) print i/10., hist[i];}}'
