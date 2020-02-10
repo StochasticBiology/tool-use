d5e6c6eca16b63e6a73256dbdf119868bddbe749
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-10 09:15:27 +0100

    Add files via upload

--- demo.sh
+++ demo.sh
@@ -11,6 +11,7 @@ gcc -o3 hypertraps-dt.c -lm -o hypertraps-dt.ce
 ./hypertraps-dt.ce total-observations.txt-trans.txt 1 1 5 0 > total-run-demo.tmp&
 
 # only run these when MCMC is complete
-#gcc -o3 posteriors.c -lm -o posteriors.ce
-#./posteriors.ce 1 total-observations.txt-trans.txt-posterior-0-1-1-5.txt > analysis-demo.txt &
+# gcc -o3 posteriors.c -lm -o posteriors.ce
+# ./posteriors.ce 1 total-observations.txt-trans.txt-posterior-0-1-1-5.txt > analysis-demo.txt &
 
+# gnuplot -e "set term svg size 400,400; unset key; set output \"demo-output.svg\"; set xlabel \"Feature\"; set ylabel \"Order\"; plot \"total-observations.txt-trans.txt-posterior-0-1-1-5.txt.process\" u 3:1:(\$5*10) ps variable pt 7; quit;"
