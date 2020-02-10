f681f21cca06d8beb1be3a7f200ec2ca1c03c520
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-10 11:24:12 +0100

    Add files via upload

--- predict.sh
+++ predict.sh
@@ -1,2 +1,5 @@
+awk 'BEGIN{n=0;}{if(n%100==0) print $0; n++;}' ../total-observations.txt-trans.txt-posterior-0-1-3-5.txt-routes.txt > total-routes-sparse.txt
 gcc predictions.c -o predictions.ce
-./predictions.ce 22 total-observations.txt total-observations.txt-trans.txt-posterior-0-1-3-5.txt-routes.txt
+#./predictions.ce 22 total-observations.txt total-observations.txt-trans.txt-posterior-0-1-3-5.txt-routes.txt
+./predictions.ce 22 total-observations.txt total-routes-sparse.txt
+
