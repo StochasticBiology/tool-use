32dc7170acc08213ed5f0ef7989ef4b5398157ac
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-10 11:44:40 +0100

    Update convergence.sh

--- convergence.sh
+++ convergence.sh
@@ -1,3 +1,4 @@
+# extract Gellman-Rubin convergence statistic from HyperTraPS output
 gcc converge.c -lm -o converge.ce
 ./converge.ce total-observations.txt-trans.txt-posterior-0-1-3-5.txt total-observations.txt-trans.txt-posterior-0-2-3-5.txt total-observations.txt-trans.txt-posterior-0-3-3-5.txt > convergence.txt
 ./parse-converge.sh convergence.txt > plot-gr.txt
