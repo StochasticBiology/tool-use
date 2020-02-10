bb1d1bf5f759755b79932b2bb88743b48a88b7b3
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-10 08:47:22 +0100

    Delete hypertraps-short.sh

--- hypertraps-short.sh
+++
@@ -1,9 +0,0 @@
-gcc -o3 hypertraps-dt.c -lm -o hypertraps-dt.ce
-
-./hypertraps-dt.ce total-names.txt-trans.txt 1 1 5 0 > total-run.tmp &
-./hypertraps-dt.ce total-convergent-trans.txt 1 1 5 0 > totalconv-run.tmp &
-./hypertraps-dt.ce wild-names.txt-trans.txt 1 1 5 0 > wild-run.tmp &
-./hypertraps-dt.ce aquatic-taxa-names.txt-trans.txt 1 1 5 0 > aquatic-run.tmp &
-./hypertraps-dt.ce nonaquatic-taxa-names.txt-trans.txt 1 1 5 0 > nonaquatic-run.tmp &
-./hypertraps-dt.ce primate-taxa-names.txt-trans.txt 1 1 5 0 > primate-run.tmp &
-./hypertraps-dt.ce nonprimate-taxa-names.txt-trans.txt 1 1 5 0 > nonprimate-run.tmp &
