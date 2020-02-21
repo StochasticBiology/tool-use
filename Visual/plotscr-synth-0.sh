3b6319cdae3992195f4f81b2907a396676fefd4a
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-21 15:46:22 +0100

    Add files via upload

--- Visual/plotscr-synth-0.sh
+++ Visual/plotscr-synth-0.sh
@@ -1,21 +1,26 @@
 # plot output of synthetic studies, random number seed 0
 
 reset
-set term svg size 800,800
+set term svg size 800,480
 set output "plotscr-synth-0.svg"
 set multiplot
-set size 0.5,0.5
+set style fill solid
+set size 0.33,1
 
 set xrange [-1:22]
 set yrange [-1:22]
 unset key
-f(x) = x*12
-set origin 0,0.5
-plot "../synth-0-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)) ps variable pt 7 lc rgbcolor "#FF8888"
-set origin 0.5,0.5
-plot "../synth-1.1-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)) ps variable pt 7 lc rgbcolor "#FF8888"
-set origin 0.,0
-plot "../synth-1.2-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)) ps variable pt 7 lc rgbcolor "#FF8888"
-set origin 0.5,0.
-plot "../synth-2-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)) ps variable pt 7 lc rgbcolor "#FF8888"
+set xlabel "Feature"
+set ylabel "Inferred ordering"
+f(x) = x*6
+set origin 0,0
+set label 1 at 1, 1 "A" front font "Arial, 24"
+plot "../synth-0-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)):(90):(270) w circles lc rgbcolor "#FF8888", "../synth-0-2.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)):(270):(90) w circles lc rgbcolor "#8888FF"
+set origin 0.33,0
+set label 1 at 1, 1 "B" front font "Arial, 24"
+plot "../synth-1.1-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)):(90):(270) w circles lc rgbcolor "#FF8888", "../synth-1.1-2.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)):(270):(90) w circles lc rgbcolor "#8888FF"
+set origin 0.66,0
+set label 1 at 1, 1 "C" front font "Arial, 24"
+plot "../synth-1.2-0.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)):(90):(270) w circles lc rgbcolor "#FF8888", "../synth-1.2-2.txt-posterior-0-1-3-5.txt.process" u 3:1:(f($5)):(270):(90) w circles lc rgbcolor "#8888FF"
+
 quit
