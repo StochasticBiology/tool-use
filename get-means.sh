265ec2898b6b9a3e32fa5915136755a0b3499792
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2020-02-22 09:32:13 +0100

    Add files via upload

--- get-means.sh
+++ get-means.sh
@@ -4,10 +4,17 @@
 
 tail -n1 ../total-observations.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-total.txt
 
-tail -n1 ../sample-observations-0.1-1-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-1.txt
-tail -n1 ../sample-observations-0.1-2-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-2.txt
-tail -n1 ../sample-observations-0.1-3-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-3.txt
+tail -n1 ../sample-observations-0.1-1.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-1.txt
+tail -n1 ../sample-observations-0.1-2.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-2.txt
+tail -n1 ../sample-observations-0.1-3.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-3.txt
+tail -n1 ../sample-observations-0.1-4.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-4.txt
+tail -n1 ../sample-observations-0.1-5.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-5.txt
+tail -n1 ../sample-observations-0.1-6.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.1-6.txt
 
-tail -n1 ../sample-observations-0.2-1-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-1.txt
-tail -n1 ../sample-observations-0.2-2-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-2.txt
-tail -n1 ../sample-observations-0.2-3-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-3.txt
+tail -n1 ../sample-observations-0.2-1.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-1.txt
+tail -n1 ../sample-observations-0.2-2.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-2.txt
+tail -n1 ../sample-observations-0.2-3.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-3.txt
+tail -n1 ../sample-observations-0.2-4.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-4.txt
+tail -n1 ../sample-observations-0.2-5.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-5.txt
+tail -n1 ../sample-observations-0.2-6.txt-trans.txt-posterior-0-1-3-5.txt.process | sed 's/#//g' | sed 's/)//g' | sed 's/,/\n/g' | sed 's/feature_//g' > means-sample-0.2-6.txt
+
